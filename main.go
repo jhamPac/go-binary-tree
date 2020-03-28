5e1ae5ce55fae9a8d63d261aeb3607f60be07c31
kevia <kevia.cloud@endurance.com>
2020-03-28 10:39:35 -0700

    basic print node function

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import "fmt"
 
+type Node struct {
+	Value int
+	Left  *Node
+	Right *Node
+}
+
+func printNode(n *Node) {
+	fmt.Print("Value: ", n.Value)
+
+	if n.Left != nil {
+		fmt.Print(" Left: ", n.Left.Value)
+	}
+
+	if n.Right != nil {
+		fmt.Print(" Right: ", n.Right.Value)
+	}
+
+	fmt.Println()
+}
+
+func main() {
+	test := &Node{Value: 9}
+	right := &Node{Value: 11}
+
+	test.Right = right
+	printNode(test)
 }
